c47c6b07c1efed78236819b6391f128cded3b3d9
MilkManngo <93600557+MilkManngo@users.noreply.github.com>
2021-11-16 23:50:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*
+
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
@@ -9,64 +9,79 @@ using namespace std;
 
 int main()
 {
-//    float arr[20];
+   float arr[20];
 
-//    for (int i=0; i<20; i++) {
-//        arr [i] = (rand() % 900 + 100)*0.1;
-//    }
+   for (int i=0; i<20; i++) {
+       arr [i] = (rand() % 900 + 100)*0.1;
+   }
 
-//    for (int i=0; i<20; i++) {
-//        cout << "arr [" << i << "] = " << arr[i] << endl;
-//    }
-
-
+   for (int i=0; i<20; i++) {
+       cout << "arr [" << i << "] = " << arr[i] << endl;
+   }
 
 
-//       for(int k=1; k<20; k++)
-//       {
-//           float temp = arr[k];
-//           int j= k-1;
-//           while(j>=0 && temp <= arr[j])
-//           {
-//               arr[j+1] = arr[j];
-//               j = j-1;
-//           }
-//           arr[j+1] = temp;
-//       }
-//       cout<<"\nSorted list is \n";
-//       for(int i=0;i<20;i++)
-//       {
-//           cout <<arr[i]<<"\t"<<endl;
-//       }
 
-    //excercise 2
 
-//       int arra[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-//       float number;
-//       cout << "Please enter a float value: "<< endl;
-//       cin >> number;
-//       for (int i=0; i<19;i++) {
-//           if ((arra[i]-0.5<=number) && (number<arra[i+1]-0.5)) {
-//               cout << "Number closest to chosen in an array of numbers from 1 to 20 is: " << arra[i] << endl;
-//               break;
+      for(int k=1; k<20; k++)
+      {
+          float temp = arr[k];
+          int j= k-1;
+          while(j>=0 && temp <= arr[j])
+          {
+              arr[j+1] = arr[j];
+              j = j-1;
+          }
+          arr[j+1] = temp;
+      }
+      cout<<"\nSorted list is \n";
+      for(int i=0;i<20;i++)
+      {
+          cout <<arr[i]<<"\t"<<endl;
+      }
+    return 0;
+}
 
-//           }
-//           else if (number>arra[19]) {
-//               cout << "Number closest to chosen in array is: 20" << endl;
-//               break;
-//           }
-//           else if (number<arra[0]){
-//                cout << "Number closest to chosen in array is: 1" << endl;
-//                break;
-//           }
-//           else {continue;}
-//       }
+=================================================================================================================================================================================
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+      int arra[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+      float number;
+      cout << "Please enter a float value: "<< endl;
+      cin >> number;
+      for (int i=0; i<19;i++) {
+          if ((arra[i]-0.5<=number) && (number<arra[i+1]-0.5)) {
+              cout << "Number closest to chosen in an array of numbers from 1 to 20 is: " << arra[i] << endl;
+              break;
+
+          }
+          else if (number>arra[19]) {
+              cout << "Number closest to chosen in array is: 20" << endl;
+              break;
+          }
+          else if (number<arra[0]){
+               cout << "Number closest to chosen in array is: 1" << endl;
+               break;
+          }
+          else {continue;}
+      }
 
 
 
     return 0;
-} */
-/*
+} 
+
+=================================================================================================================================================================================
+
 #include<iostream>
 #include <string>
 
@@ -116,8 +131,9 @@ int main()
 
     return 0;
 }
-*/
-/*
+
+=================================================================================================================================================================================
+
 #include <iostream>
 using namespace std;
 void printIndex(string str, string s)
@@ -146,6 +162,8 @@ int main()
     return 0;
 } */
 
+=================================================================================================================================================================================    
+    
 #include <string>
 #include <iostream>
 #include <stdio.h>
